e99e55bc5e075b377124fe1053c387304b727446
sna145815 <sna145815@gmail.com>
2024-04-05 11:38:57 +0900

    vidio DB Con AND LOG CLICKED VIDIO OPEN

--- GUI/findPeopleGUI.py
+++ GUI/findPeopleGUI.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from PyQt5.QtCore import QThread, pyqtSignal, Qt,QTimer
-from PyQt5.QtWidgets import QMainWindow, QApplication,QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication,QTableWidgetItem,QMessageBox
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5 import uic,QtWidgets
 import socket
@@ -13,7 +13,7 @@ from datetime import datetime
 
 
 from_class = uic.loadUiType("findPeopleGUI.ui")[0]
-HOST = "192.168.0.32"
+HOST = "192.168.0.40"
 # MySQL 서버에 대한 연결 설정
 connection = mysql.connector.connect(
                 host="192.168.0.40",
@@ -102,38 +102,81 @@ class MainWindow(QMainWindow, from_class):
         
         self.PersonADD.clicked.connect(self.insert_person)
         self.pictureUpload.clicked.connect(self.fileopen)
+        self.LogEdit.itemDoubleClicked.connect(self.play_video)
         
+    
+    def play_video(self,item):
+        row = item.row()
+        item_id = int(self.LogEdit.item(row, 0).text())  # 첫 번째 열에 있는 ID를 가져옴
+
+        # 데이터베이스에서 해당 ID의 VIDEO_PATH 가져오기
+        cursor = connection.cursor()
+        query = "SELECT VIDEO_PATH FROM REC_VIDIO WHERE LOG_ID = %s"
+        cursor.execute(query, (item_id,))
+        result = cursor.fetchone()
+        cursor.close()
+
+        if result:
+            video_path = result[0]
+            # OpenCV를 사용하여 동영상 재생
+            cap = cv2.VideoCapture(video_path)
+            if not cap.isOpened():
+                QMessageBox.warning(self, "Error", "동영상을 재생할 수 없습니다.")
+                return
+            
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                cv2.imshow("Video", frame)
+                if cv2.waitKey(30) & 0xFF == ord('q'):  # 'q' 키를 누르면 종료
+                    break
+            
+            cap.release()
+            cv2.destroyAllWindows()
+        else:
+            QMessageBox.warning(self, "Error", "해당 ID에 대한 동영상이 없습니다.")
+
     def updateResult(self,color):
         global person_data
         count = 0
         self.resultColor.setText(color)
-
+        name = None
         for data in person_data:
             if color in data[3]:
-                print("Color found in global data:", color)
+                name = data[0]
                 count = count+1
                 break
-        else:
-            print("Color not found in global data:", color)
 
-        
+
         if count == 1:
             if not self.recvidio:
                 self.recvidio = True
+                self.insert_log('의심', data[0])
                 print("녹화시작")
-                self.start_recording()  # 녹화 시작
-            self.insert_log('의심', data[0])
+                self.start_recording(name) # 녹화 시작
+        elif count == 3:
+            if not self.recvidio:
+                self.recvidio = True
+                self.insert_log('강력', data[0])
+                print("녹화시작")
+                self.start_recording(name) # 녹화 시작         
         else:
             if self.recvidio:
                 print("녹화종료")
-                self.stop_recording()  # 녹화 종료
-            self.recvidio = False
+                self.stop_recording() # 녹화 종료
+                self.recvidio = False
 
-    def start_recording(self):
+    def start_recording(self,name):
         if not self.recording:
             self.recording = True
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+            vidio_path = '../REC/'+formatted_time+'_'+name+'.avi'
             fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 코덱 설정
-            self.video_writer = cv2.VideoWriter('RightNow.avi', fourcc, 20.0, (1200, 300))  # VideoWriter 객체 생성
+            self.video_writer = cv2.VideoWriter(vidio_path, fourcc, 20.0, (1200, 300))  # VideoWriter 객체 생성
+            self.insert_vidiopath(vidio_path)
 
     def stop_recording(self):
         if self.recording:
@@ -212,14 +255,25 @@ class MainWindow(QMainWindow, from_class):
     def select_log(self):
             # 데이터베이스에서 모든 레코드 가져오기
             cur = connection.cursor()
-            cur.execute("SELECT * FROM LOG")
+            cur.execute("SELECT ID, FINDTIME, ACCURACY,NAME FROM LOG")
             logs = cur.fetchall()
-            log_text = ""
 
-            for log in logs:
-                log_text += " ".join(map(str, log)) + "\n"  # 각 로그를 문자열로 변환하여 log_text에 추가
+            self.LogEdit.setRowCount(len(logs))
+            self.LogEdit.setColumnCount(3)
+            headers = ['ID','FINDTIME', 'ACC', 'NAME']
+            self.LogEdit.setHorizontalHeaderLabels(headers)
+
+            # 데이터 채우기
+            for row_num, row_data in enumerate(logs):
+                for col_num, cell_data in enumerate(row_data):
+                    self.LogEdit.setItem(row_num, col_num, QTableWidgetItem(str(cell_data)))
+
+            # 연결 종료
+            cur.close()
             
-            self.LogEdit.setText(log_text)  # LogEdit에 로그 텍스트 설정
+                
+            
+            
 
 
     
@@ -236,7 +290,20 @@ class MainWindow(QMainWindow, from_class):
         # 연결 종료
         cursor.close()
         self.select_log()
-        
+
+    
+    def insert_vidiopath(self, path):
+        # SQL 쿼리 실행
+        cursor = connection.cursor()
+        query = """
+            INSERT INTO REC_VIDIO (VIDEO_PATH, LOG_ID)
+            VALUES (%s, (SELECT ID FROM LOG ORDER BY FINDTIME DESC LIMIT 1))
+        """
+        values = (path,)
+        cursor.execute(query, values)
+        connection.commit()
+        cursor.close()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
